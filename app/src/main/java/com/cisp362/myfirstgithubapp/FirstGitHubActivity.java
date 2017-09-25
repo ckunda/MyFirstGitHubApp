f84c59728363ad34a5086ed2e73f4261ce63a271
ckunda <ckunda@hotmail.com>
2017-09-25 15:24:21 -0700

    Initial commit

---
+++ app/src/main/java/com/cisp362/myfirstgithubapp/FirstGitHubActivity.java
@@ -0,0 +1,13 @@
+package com.cisp362.myfirstgithubapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstGitHubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_git_hub);
+    }
+}
